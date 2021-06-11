27e1407d98407f4ca7d0d04a072ed748dc082053
supermanc88 <supermanc88@gmail.com>
2021-06-11 18:55:01 +0800

    update

--- lua/typeofdata.lua
+++ lua/typeofdata.lua
@@ -20,7 +20,7 @@ print(type(type(X)))            --> string
 
 -- nil 空
 -- 对于全局变量和table，nil还有一个删除作用，给它们赋值，等同于把它们删掉
-print("********************************")
+print("******************************** nil")
 tab1 = {key1 = "val1", key2 = "val2", "val3"}
 for k, v in pairs(tab1) do
 	print(k .. " - " .. v)
@@ -33,6 +33,25 @@ end
 
 -- nil 作比较时需要加上双引号
 -- type(X)==nil 结果为 false 的原因是 type(X) 实质是返回的 "nil" 字符串，是一个 string 类型
-print("********************************")
+print("******************************** nil")
 print(type(X) == nil)			--> false
 print(type(X) == "nil")			--> true
+
+-- boolean
+-- boolean 只有ture/false,lua把false和nil看作是false，其它的都是true，数字0也是true
+print("******************************* boolean")
+print(type(true))               --> boolean
+print(type(false))               --> boolean
+print(type(nil))               --> nil
+
+if false or nil then
+    print("至少一个为true")
+else
+    print("false和nil都为false")	--> false 和 nil 都为 false
+end
+
+if 0 then
+    print("数字0为true")		--> 数字 0 是 true
+else
+    print("数字0为false")
+end
