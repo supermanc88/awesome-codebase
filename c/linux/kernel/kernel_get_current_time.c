156a61d4b41781335dfea90328970081b4a69a70
supermanc88 <supermanc88@gmail.com>
2021-06-25 10:16:23 +0800

    update linux kernel get current time

--- c/linux/kernel/kernel_get_current_time.c
+++ c/linux/kernel/kernel_get_current_time.c
@@ -4,6 +4,37 @@
 #include <linux/time.h>
 
 
+/**
+* \require <linux/time.h>
+* \brief gettime1 使用 do_gettimeofday 在内核版本5中已不在支持
+*/
+void gettime1(void)
+{
+    struct timeval now;
+
+    do_gettimeofday(&now);
+
+    // kern_time_init timeval struct size = 16, sec size = 8, usec size = 8
+
+    printk("%s timeval struct size = %llu, sec size = [ %llu ], usec size = [ %llu ]\n", __func__, sizeof (now), sizeof (now.tv_sec), sizeof (now.tv_usec));
+
+    printk("%s current time = %lu.%lu(secs.usecs)\n", __func__, now.tv_sec, now.tv_usec);
+}
+
+
+/**
+* \require <linux/time.h>
+* \brief gettime2 这个函数是相对来说比较通用的，在内核3和5中都可使用
+*/
+void gettime2(void)
+{
+	struct timespec64 now = {0};
+
+	ktime_get_ts64(&now);
+
+	printk("%s struct size = [ %llu ], sec = [%lld], nsec = [%ld]\n", __func__, sizeof(now), now.tv_sec, now.tv_nsec);
+}
+
 
 /**
 * \require <linux/time.h>
@@ -12,16 +43,7 @@
 */
 static int __init kern_time_init(void)
 {
-
-    struct timeval now;
-
-    do_gettimeofday(&now);
-
-    // kern_time_init timeval struct size = 16, sec size = 8, usec size = 8
-
-    printk("%s timeval struct size = %d, sec size = %d, usec size = %d\n", __func__, sizeof (now), sizeof (now.tv_sec), sizeof (now.tv_usec));
-
-    printk("%s current time = %lu.%lu(secs.usecs)\n", __func__, now.tv_sec, now.tv_usec);
+	gettime1();
 
     return 0;
 }
